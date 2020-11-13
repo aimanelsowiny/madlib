0688f46d44dc72dba72f33f4101479f286d84317
Aiman <aiman.elsowiny@gmail.com>
2020-11-13 15:41:33 -0500

    uploaded madlib python file

---
+++ madlib.py
@@ -0,0 +1,34 @@
+
+print("Hello, welcome to Space Exploration Mad Lib. Please fill out this application so we can update your profile.")
+
+
+name = input("What is your name? ")
+
+sillyword = input("Enter a silly word: ")
+adjective = input("Enter a adjective: ")
+number = input("Enter any number: ")
+emotion = input("Enter a emotion: ")
+
+noun = input("Enter a noun: ")
+
+relativename = input("Enter a relative's name: ")
+
+adjective2 = input("Enter a second adjective: ")
+verb = input("Enter a verb ")
+emotion2 = input("Finally, enter a happy emotion: ")
+
+print("Here take a look at your profile")
+
+print("Hello my name is astronaut " + name + ",")
+print("I am on my way to planet " + sillyword + ".")
+print("I will be gone for " + number + " days.")
+
+print("I am very " + adjective + " about the trip but, i will deeply miss my " + noun)
+
+print("I have heard that the atmosphere there is " + adjective2)
+print("Luckily my relative " + relativename + " packed me a jacket to keep me " + adjective)
+
+print("When I land on planet " + sillyword + " I will " + verb + " for joy")
+
+print("I am " + emotion + " to walk on another planet. I could not be more " + emotion2 + " for this trip!")
+
